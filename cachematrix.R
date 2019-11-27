d6fb2dedcee5d5da58c6d3a46dd3d01d6de5dd0c
vbkaran <vbk.singh@gmail.com>
2019-11-27 13:51:29 +0100

    fifth

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix(sample(1:200,10),3,3)) {
-  s <- NULL
+  m <- NULL
   set <- function(y) {
     x <<- y
-    s <<- NULL
+    m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) s <<- mean
-  getmean <- function() s
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
   list(set = set, get = get,
        setmean = setmean,
        getmean = getmean)
 }
 ##
 cachemean <- function(x, ...) {
-  s <- x$getmean()
-  if(!is.null(s)) {
+  m <- x$getmean()
+  if(!is.null(m)) {
     message("getting inversed matrix")
-    return(s)
+    return(m)
   }
   data <- x$get()
-  s <- mean(data, ...)
-  x$setmean(s)
-  s
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
 
