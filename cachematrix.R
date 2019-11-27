c82cadb5cfcec564a2cb8fef630079c66d8dfd4e
vbkaran <vbk.singh@gmail.com>
2019-11-27 13:48:03 +0100

    fouth

--- cachematrix.R
+++ cachematrix.R
@@ -10,22 +10,22 @@ makeCacheMatrix <- function(x = matrix(sample(1:200,10),3,3)) {
     s <<- NULL
   }
   get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
+  setmean <- function(mean) s <<- mean
+  getmean <- function() s
   list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+       setmean = setmean,
+       getmean = getmean)
 }
 ##
-## Same here, changed "mean" to "solve" and "m" to "s"
-cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
+cachemean <- function(x, ...) {
+  s <- x$getmean()
   if(!is.null(s)) {
     message("getting inversed matrix")
     return(s)
   }
   data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
+  s <- mean(data, ...)
+  x$setmean(s)
   s
 }
+
